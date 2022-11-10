08795209e0aa9bf68c6e88d032fe5e91000356fc
Kapara04 <markonikolicmn13@gmail.com>
2022-11-10 18:59:14 +0100

    gotovo

--- Contact.aspx
+++ Contact.aspx
@@ -1,7 +1,31 @@
 ﻿<%@ Page Title="Contact" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Contact.aspx.cs" Inherits="TuristickaAgencija.Contact" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-    <h1>PPPSADPASDP</h1>
+    <h1>Prikaz mesta</h1>
 
+    
+    <p> Izaberite mesto: </p>
+    <asp:DropDownList ID="ddlMesto" runat="server"></asp:DropDownList>
 
+    <asp:Button ID="btnTrazi" runat="server" Text="Trazi" OnClick="btnTrazi_Click" />
+    <br /> <br />
+
+    <table>
+        <tr>
+            <td>Ime: </td>
+            <td> <asp:Label ID="lblIme" runat="server" Text=""></asp:Label></td>
+        </tr>
+        <tr>
+            <td>Prezime:  </td>
+            <td> <asp:Label ID="lblPrezime" runat="server" Text=""></asp:Label></td>
+        </tr>
+        <tr>
+            <td>Datum polaska: </td>
+            <td> <asp:Label ID="lblPolazak" runat="server" Text=""></asp:Label></td>
+        </tr>
+        <tr>
+            <td>Datum dolaska:  </td>
+            <td> <asp:Label ID="lblDolazak" runat="server" Text=""></asp:Label></td>
+        </tr>
+    </table>
 </asp:Content>
--- Contact.aspx.cs
+++ Contact.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace TuristickaAgencija
 {
@@ -11,7 +12,45 @@ namespace TuristickaAgencija
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!Page.IsPostBack)
+            {
+                ddlMesto.Items.Add(new ListItem("", ""));
+                ddlMesto.Items.Add(new ListItem("Herceg Novi"));
+                ddlMesto.Items.Add(new ListItem("Beograd"));
+                ddlMesto.Items.Add(new ListItem("Novi Sad"));
+                ddlMesto.Items.Add(new ListItem("Trebinje"));
+            }
+        }
 
+        protected void btnTrazi_Click(object sender, EventArgs e)
+        {
+            string trazenigrad = ddlMesto.SelectedValue;
+            lblIme.Text = trazenigrad;
+            Fill(trazenigrad);
+
+
+        }
+
+        public void Fill(string grad)
+        {
+            using (SqlConnection conn = new SqlConnection(Connection.conString))
+            {
+                conn.Open();
+                string cmdSelect = "SELECT * from Mesto join Putuje on Mesto.IdMesto = Putuje.IdMesto join Putnik on Putnik.JMBG = Putuje.JMBG where Mesto.Grad = '" + grad + "'";
+
+                using(SqlCommand cmd = new SqlCommand(cmdSelect, conn))
+                {
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while(reader.Read())
+                    {
+                        lblIme.Text = reader["Ime"].ToString();
+                        lblPrezime.Text = reader["Prezime"].ToString();
+                        lblPolazak.Text = reader["DatumDolaska"].ToString();
+                        lblDolazak.Text = reader["DatumOdlaska"].ToString();
+
+                    }
+                }
+            }
         }
     }
 }
