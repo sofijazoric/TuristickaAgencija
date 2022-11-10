267d3bc75a550342bba42b65e2c221e22b7d0027
borisstepanovic <rockstarboris04@gmail.com>
2022-11-10 16:42:17 +0100

    kajhfa

--- Putnik.cs
+++ Putnik.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.SqlClient;
 
 namespace TuristickaAgencija
 {
@@ -22,5 +23,24 @@ namespace TuristickaAgencija
        
         public Putnik() { }
 
+        public void Prikaz(ParametriZaPretragu parametri)
+        {
+            string SQLSelect = "Select * from Putnik where JMBG = '" + parametri.JMBG + "'";
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = Connection.conString;
+
+            SqlCommand cmd = new SqlCommand(SQLSelect, con);
+            SqlDataReader reader;
+
+            using(con)
+            {
+                try
+                {
+                    con.Open();
+                    reader = cmd.ExecuteReader();
+                    reader.Read()
+                }
+            }
+        }
     }
 }
